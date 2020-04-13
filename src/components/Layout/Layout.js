0fc5b0b8303731932a64d7fd40d96beb5fddf91d
samlak <samlak1999@gmail.com>
2020-04-14 00:26:59 +0100

    SideDrawer and Logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxillary';
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const Layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrawer, BackDrop</div>
-        <main>
+        <Toolbar />
+        <SideDrawer />
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
